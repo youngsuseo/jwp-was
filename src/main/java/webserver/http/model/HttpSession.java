62abcb2670cc9ce19603aa99aa1be09e56c17d41
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-08 23:14:37 +0900

    stash4_0808_2314

--- src/main/java/webserver/http/model/HttpSession.java
+++ src/main/java/webserver/http/model/HttpSession.java
@@ -1,26 +1,46 @@
 package webserver.http.model;
 
-public class HttpSession {
+import java.util.HashMap;
+import java.util.Map;
 
+public class HttpSession {
     private String id;
+    private SessionAttribute attributes;
+
+    public HttpSession(String id) {
+        this(id, new HashMap<>());
+    }
+
+    public HttpSession(String id, Map<String, Object> attributes) {
+        this(id, new SessionAttribute(attributes));
+    }
+
+    public HttpSession(String id, SessionAttribute attributes) {
+        this.id = id;
+        this.attributes = attributes;
+    }
 
     public String getId() {
         return id;
     }
 
     public void setAttribute(String name, Object value) {
-
+        attributes.setAttribute(name, value);
     }
 
     public Object getAttribute(String name) {
-        return null;
+        return attributes.getAttribute(name);
     }
 
     public void removeAttribute(String name) {
-
+        attributes.removeAttribute(name);
     }
 
     public void invalidate() {
+        attributes.invalidate();
+    }
 
+    Map<String, Object> getAttributes() {
+        return attributes.getAttributes();
     }
 }
--- src/main/java/webserver/http/model/HttpSessions.java
+++ src/main/java/webserver/http/model/HttpSessions.java
@@ -1,12 +1,23 @@
 package webserver.http.model;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class HttpSessions {
     private Map<String, HttpSession> httpSessionMap;
 
+    public HttpSessions() {
+        this(new HashMap<>());
+    }
+
     public HttpSessions(Map<String, HttpSession> httpSessionMap) {
         this.httpSessionMap = httpSessionMap;
+    }
+
+    public void create(HttpSession httpSession) {
+        String uuid = UUID.randomUUID().toString();
+        httpSessionMap.put(uuid, httpSession);
     }
 
     public Map<String, HttpSession> getHttpSessionMap() {
